5f4bfd75d90a3754f53b36a929c0785f3fd9769a
Kevin <devkevinsoria@gmail.com>
2022-06-24 02:11:21 -0300

    instalando Tailwind y creando sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Sidebar from "./components/Sidebar";
 import "./App.css";
 
 function App() {
    return (
       <div>
-         <h1>Personal Portfolio</h1>
+         <h1 className="text-3xl font-bold underline">Personal Portfolio</h1>
+         <Sidebar />
       </div>
    );
 }

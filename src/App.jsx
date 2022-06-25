5827695775601ef6c8ef87a3c17c23a050ff1e0d
Kevin <devkevinsoria@gmail.com>
2022-06-24 21:01:32 -0300

    bg y dimenciones del contenedor main listo

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import "./App.css";
 
 function App() {
    return (
-      <div>
-         <h1 className="text-3xl font-bold underline">Personal Portfolio</h1>
+      <div className="max-w-full max-h-full h-screen bg-gray-900 ">
+         <h1 className="text-3xl font-bold underline text-gray-300">
+            Personal Portfolio
+         </h1>
          <Sidebar />
       </div>
    );

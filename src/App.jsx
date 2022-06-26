59d695ce52ae6368270db32372c5109f8d7e206d
Kevin <devkevinsoria@gmail.com>
2022-06-26 02:42:43 -0300

    previo a instalar react iconos

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import "./App.css";
 
 function App() {
    return (
-      <div className="max-w-full max-h-full h-screen bg-gray-900 ">
+      <div className="max-w-full max-h-full h-screen flex bg-gray-900 ">
+         <Sidebar />
          <h1 className="text-3xl font-bold underline text-gray-300">
             Personal Portfolio
          </h1>
-         <Sidebar />
       </div>
    );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,37 @@
+import Logo from "../assets/baka.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { house } from "@fortawesome/free-solid-svg-icons";
+
+/*
+***** Cosas para hacer:
+* Cambiar Logo.
+* Crear un componente para cada elemento de la <li>
+* Incluir icono por cada elemento.
+* Dark mode
+
+*/
 const Sidebar = () => {
-   return <div></div>;
+   return (
+      <div className="w-1/5 max-w-xs h-full bg-gray-300 text-gray-800 dark:bg-gray-800 dark:text-gray-200">
+         <div className="flex flex-col justify-evenly items-center h-screen box-shadow">
+            <div className="w-2/3 h-auto">
+               <img src={Logo} alt="Logo" />
+            </div>
+            <div className="text-lg border-2 border-white p-6">
+               <ul>
+                  <li>
+                     <FontAwesomeIcon icon={house} />
+                     Home
+                  </li>
+                  <li>About</li>
+                  <li>Services</li>
+                  <li>Portfolio</li>
+                  <li>Contact</li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default Sidebar;

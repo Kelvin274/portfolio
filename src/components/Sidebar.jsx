3c231733d527e96f7c8a837131e226b2cc572379
Kevin <devkevinsoria@gmail.com>
2022-06-28 23:41:17 -0300

    hotfix burgerMenu display

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,39 +18,34 @@ const Sidebar = () => {
    };
 
    let burguerMenuProps =
-      "text-bold text-3xl p-3 cursor-pointer md:hidden hover:text-gray-100";
+      "text-bold text-3xl p-3 cursor-pointer absolute md:hidden hover:text-gray-100";
 
    return (
-      <>
-         {isOpen && (
-            <div
-               className={`${burguerMenuProps} fixed left-6 top-3 z-10 bg-gray-800/60 hover:bg-gray-800 rounded-full`}
-            >
-               <Open onClick={handleOpen} />
-            </div>
-         )}
-         <section
-            className={`w-[200px] md:w-1/5 h-screen relative ${
-               isOpen ? "left-[-200px]" : "left-0"
-            } md:left-0 md:static shadow-lg bg-gray-800
-            ease-in-out duration-300`}
+      <section
+         className={`w-[200px] md:w-1/5 h-screen relative ${
+            isOpen ? "left-[-200px]" : "left-0"
+         } md:left-0 md:static shadow-lg bg-gray-800
+            ease-in-out duration-500`}
+      >
+         <div
+            className={`${burguerMenuProps} left-[14rem] top-3 z-10 transition-opacity ${
+               isOpen ? "opacity-100 ease-in delay-500" : "opacity-0 ease-out"
+            }
+             bg-gray-800/60 hover:bg-gray-800 rounded-full`}
          >
-            {!isOpen && (
-               <div
-                  className={`${burguerMenuProps} absolute left-[9.5rem] top-2`}
-               >
-                  <Close onClick={handleOpen} />
-               </div>
-            )}
+            <Open onClick={handleOpen} />
+         </div>
+         <div className={`${burguerMenuProps} left-[9.5rem] top-2`}>
+            <Close onClick={handleOpen} />
+         </div>
 
-            <div className="flex flex-col h-full md:mt-0 justify-evenly items-center">
-               <div className="w-36 h-auto  md:mt-0 md:w-2/3">
-                  <img src={Logo} alt="Logo" />
-               </div>
-               <SidebarMenu />
+         <div className="flex flex-col h-full md:mt-0 justify-evenly items-center">
+            <div className="w-36 h-auto  md:mt-0 md:w-2/3">
+               <img src={Logo} alt="Logo" />
             </div>
-         </section>
-      </>
+            <SidebarMenu />
+         </div>
+      </section>
    );
 };
 

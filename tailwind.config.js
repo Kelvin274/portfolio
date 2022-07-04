525bb5b782b2a5c9d9395a0b81bc25668de04571
Kevin <devkevinsoria@gmail.com>
2022-07-03 21:46:30 -0300

    Homepage creado y ajustando colores css para futuras variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
    content: ["./src/**/*.{js,jsx,ts,tsx}"],
    theme: {
       extend: {
+         screens: {
+            xs: { min: "362px" },
+            ...defaultTheme.screens,
+         },
          fontFamily: {
             poppins: ["Poppins", "sans-serif"],
+            clickerScript: ["Clicker-Script", "cursive"],
          },
       },
+      plugins: [],
    },
-   plugins: [],
 };
